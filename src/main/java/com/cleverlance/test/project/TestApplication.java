36ec74993ea96c32ea19837c0094864889fefd9b
E_TMICHA <tomas_michalicka@kb.cz>
2023-06-07 12:23:11 +0200

    component scan fix

--- src/main/java/com/cleverlance/test/project/TestApplication.java
+++ src/main/java/com/cleverlance/test/project/TestApplication.java
@@ -8,8 +8,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = "com.example.Test")
-
 public class TestApplication {
 	
 	public static void main(String[] args) {
